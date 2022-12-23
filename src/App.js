c7c1cfd8b35454d2c5ef5348ada02e1d80bfde8e
rrezHetemi <rrezart.hetemi@outlook.com>
2022-12-23 11:35:53 +0100

    added project files

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import Search from "./components/Search";
+import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import {GiKnifeFork} from "react-icons/gi"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={'/'}> 
+            Tasteyyy
+          </Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+`
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }
+`
+
+export default App;

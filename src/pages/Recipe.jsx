b1c0d1ae9d017e9c6f97a98c41636187849fb5cb
rrezHetemi <rrezart.hetemi@outlook.com>
2022-12-28 20:54:50 +0100

    changes for responsivity on recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -12,7 +12,6 @@ function Recipe() {
     const data = await fetch(`
     https://api.spoonacular.com/recipes/${params.id}/information?apiKey=${process.env.REACT_APP_API_KEY}`
     );
-    console.log(params.id)
     const detailData = await data.json();
     setDetails(detailData);
   }
@@ -65,21 +64,25 @@ const DetailWrapper = styled.div`
   margin-top: 10rem;
   margin-bottom: 1rem;
   display: flex;
+  justify-content: center;
+  justify-items: space-between;
   .active{
     background: linear-gradient(35deg, #494949, #313131);
     color: white;
   }
   h2{
     margin-bottom: 2rem;
+    max-width: 50%;
   }
   li{
-    font-size: 1.2rem;
+    font-size: 1rem;
     line-height: 2.5rem;
   }
   ul{
     margin: 2rem 1rem;
   }
   img{
+    max-width: 20vw;
     border-radius: 1rem;
   }
   button{
@@ -106,7 +109,7 @@ const Button = styled.button`
   color: #313131;
   background: white;
   border: 2px solid black;
-  margin-right: 2rem;
+  margin: 0.5rem 1rem;
   font-weight: 600;
   border-radius: 1rem;
   @media screen and (max-width: 640px) {
@@ -115,7 +118,12 @@ const Button = styled.button`
 `
 
 const Info = styled.div`
-  margin-left: 10rem;
+  margin-left: 1rem;
+  //.max-width: 20vw;
+  width: clamp(50%, 50%, 20vw);
+  h3{
+    font-size: 1rem;
+  }
   @media screen and (max-width: 640px) {
     display: flex;
     flex-direction: column;
